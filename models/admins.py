80b5d125322a81a3fccded63853cb32abbd54c01
Jorn Kalu <kiceej@gmail.com>
2024-02-28 16:38:16 +0100

    debug

--- models/admins.py
+++ models/admins.py
@@ -68,7 +68,7 @@ def get_anon_admin_by_id(db: Session, id: int=0):
     return db.query(Admin).filter(Admin.id == id).first()
 
 def get_admins(db: Session):
-    return get_query(db=db).filter(Admin.deleted_at == None).order_by(desc(Admin.id))
+    return db.query(Admin).filter(Admin.deleted_at == None).order_by(desc(Admin.id))
 
 def admin_login(db: Session, field: str=None):
     return get_query(db=db).filter(and_(or_(Admin.email == field, Admin.username == field), Admin.deleted_at == None)).first()

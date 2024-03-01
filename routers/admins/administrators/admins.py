6bce89194a41acb0bb131ca7498d1821b9fd3157
Jorn Kalu <kiceej@gmail.com>
2024-03-01 15:32:22 +0100

    debug

--- routers/admins/administrators/admins.py
+++ routers/admins/administrators/admins.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Request, Depends, HTTPException
 from modules.authentication.auth import auth
 from modules.admin.manage import retrieve_admins, retrieve_single_admin
 from database.schema import AdminModel, AdminNeoModel, AdminDetailResponseModel, ErrorResponse
-from database.db import get_session
+from database.db import get_session, get_db
 from sqlalchemy.orm import Session
 from fastapi_pagination import LimitOffsetPage, Page
 
@@ -13,7 +13,7 @@ router = APIRouter(
 )
 
 @router.get("/get_all", response_model=Page[AdminNeoModel], responses={404: {"model": ErrorResponse}, 401: {"model": ErrorResponse}, 403: {"model": ErrorResponse}})
-async def get_all(request: Request, admin=Depends(auth.auth_admin_wrapper), db: Session = Depends(get_session)):
+async def get_all(request: Request, admin=Depends(auth.auth_admin_wrapper), db: Session = Depends(get_db)):
     return retrieve_admins(db=db)
 
 @router.get("/get_single/{admin_id}", response_model=AdminDetailResponseModel, responses={404: {"model": ErrorResponse}, 401: {"model": ErrorResponse}, 403: {"model": ErrorResponse}})
